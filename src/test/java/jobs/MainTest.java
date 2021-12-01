159fa73aa9d05dbe93587a9d2338e9f3a862a2a8
akky <>
2021-12-02 06:08:14 +0900

    各テスト実装

--- src/test/java/jobs/MainTest.java
+++ src/test/java/jobs/MainTest.java
@@ -1,14 +1,17 @@
 package jobs;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
 import org.junit.jupiter.api.Test;
 
 public class MainTest {
     @Test
     public void test_main() {
-        String[] args = null;
-        Main.main(args);
-        assertTrue(true);
+        try {
+            String[] args = null;
+            Main.main(args);
+        } catch (Exception e) {
+            fail();
+        }
     }
 }
